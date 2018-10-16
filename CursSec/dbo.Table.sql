660df614409c44b09478d60eccb046edc01db4a9
mrlead <krendelek1509999@gmail.com>
2018-10-17 00:56:31 +0300

    Идёт работа над локальной БД, для хранения писем.

---
+++ CursSec/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [name] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_Users_ToUser_settings] FOREIGN KEY ([name]) REFERENCES [User_settings]([user])
+)
